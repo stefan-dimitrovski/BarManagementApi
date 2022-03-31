7fe064e6d2d9f6632dd0a399d12523637962316f
Stefan Dimitrovski <stefan.dimitrovski_19@hotmail.com>
2022-03-31 11:29:14 +0200

    Initial commit

---
+++ src/main/kotlin/com/sorsix/barmanagmentapi/BarManagmentApiApplication.kt
@@ -0,0 +1,11 @@
+package com.sorsix.barmanagmentapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BarManagmentApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<BarManagmentApiApplication>(*args)
+}
