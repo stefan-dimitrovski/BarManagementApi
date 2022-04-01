b2b3dc964e2426d581906d5cfbc9a11ac8d797a7
andrejSkenderski <andrej.skenderski@gmail.com>
2022-04-01 14:53:31 +0200

    Table controller and service implemented

--- src/main/kotlin/com/sorsix/barmanagmentapi/api/TableController.kt
+++ src/main/kotlin/com/sorsix/barmanagmentapi/api/TableController.kt
@@ -1,17 +1,31 @@
 package com.sorsix.barmanagmentapi.api
 
+import com.sorsix.barmanagmentapi.domain.Table
 import com.sorsix.barmanagmentapi.service.TableService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/api/tables")
+@RestController
+@RequestMapping("/api/tables")
 class TableController(
     val tableService: TableService
 ) {
 
     @GetMapping
-    fun getAllTables() {
-        TODO()
+    fun getAllTables(): List<Table> = tableService.getTables()
+
+    @PutMapping("/{id}")
+    fun openTable(@PathVariable id: Long) {
+        tableService.updateIsOpen(id, true)
     }
+
+    @PutMapping("/close/{id}")
+    fun closeTable(@PathVariable id: Long) {
+        tableService.updateIsOpen(id, false)
+    }
+
 }
--- src/main/kotlin/com/sorsix/barmanagmentapi/service/TableService.kt
+++ src/main/kotlin/com/sorsix/barmanagmentapi/service/TableService.kt
@@ -1,8 +1,17 @@
 package com.sorsix.barmanagmentapi.service
 
+import com.sorsix.barmanagmentapi.domain.Table
 import com.sorsix.barmanagmentapi.repository.TableRepository
 import org.springframework.stereotype.Service
+import javax.transaction.Transactional
 
 @Service
 class TableService(val tableRepository: TableRepository) {
+
+    fun getTables(): List<Table> = tableRepository.findAll()
+
+    @Transactional
+    fun updateIsOpen(tableId: Long, isOpen: Boolean) {
+        tableRepository.updateIsOpen(tableId, isOpen)
+    }
 }
