17ab4d5dceba0d758734d1acdbc1536c386ebd9e
andrejSkenderski <andrej.skenderski@gmail.com>
2022-04-12 12:38:37 +0200

    Jwt constants updated

--- src/main/kotlin/com/sorsix/barmanagmentapi/config/jwt/JwtConstants.kt
+++ src/main/kotlin/com/sorsix/barmanagmentapi/config/jwt/JwtConstants.kt
@@ -2,11 +2,11 @@ package com.sorsix.barmanagmentapi.config.jwt
 
 class JwtConstants {
     companion object {
-        const val SECRET: String = "s3cr3tt0k3nsadasdashdvbasdhjadaksjfdasfhvh3cr3tt0k3ns" +
+        const val SECRET = "s3cr3tt0k3nsadasdashdvbasdhjadaksjfdasfhvh3cr3tt0k3ns" +
                 "adasdashdvbasdhjadaksjfdasfhvh3cr3tt0k3nsadasdashdvb" +
                 "asdhjadaksjfdasfhvh3cr3tt0k3nsadasdashdvbasdhjadaksjfdasfhvh"
-        const val EXPIRATION_TIME: Int = 864_000_000 //10 days
-        const val TOKEN_PREFIX: String = "Bearer "
-        const val HEADER_STRING: String = "Authorization"
+        const val EXPIRATION_TIME: Long = 86400000L //1 day
+        const val TOKEN_PREFIX = "Bearer "
+        const val HEADER_STRING = "Authorization"
     }
 }
