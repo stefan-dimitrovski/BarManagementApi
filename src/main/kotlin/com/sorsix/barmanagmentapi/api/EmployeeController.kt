c2eca514b7101f3f69574806deae752b75d4678e
Stefan Dimitrovski <stefan.dimitrovski_19@hotmail.com>
2022-04-12 14:16:16 +0200

    cors;

--- src/main/kotlin/com/sorsix/barmanagmentapi/api/EmployeeController.kt
+++ src/main/kotlin/com/sorsix/barmanagmentapi/api/EmployeeController.kt
@@ -2,12 +2,14 @@ package com.sorsix.barmanagmentapi.api
 
 import com.sorsix.barmanagmentapi.domain.User
 import com.sorsix.barmanagmentapi.service.EmployeeService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/employees")
+@CrossOrigin
 class EmployeeController(private val employeeService: EmployeeService) {
 
     @GetMapping
