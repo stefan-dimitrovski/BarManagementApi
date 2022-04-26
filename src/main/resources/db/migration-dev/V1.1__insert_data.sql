e70f819d4ca5b30b04fb750b72c59a1c77f0c0a3
andrejSkenderski <andrej.skenderski@gmail.com>
2022-04-26 12:02:05 +0200

    V1.1 flyway insert data script cleanup

--- src/main/resources/db/migration-dev/V1.1__insert_data.sql
+++ src/main/resources/db/migration-dev/V1.1__insert_data.sql
@@ -1,39 +1,25 @@
 INSERT INTO public.locales (address, lat, lng, name)
 VALUES ('Boulevard Phillip the Second of Macedon 14, Skopje 1000', '41.9972546654559', '21.418529735959524',
-        'Old City House');
-INSERT INTO public.locales (address, lat, lng, name)
-VALUES ('1-vi Maj Boulevard b.b, Bitola 7000', '41.030889', '21.333390', 'Bakery Boulevard');
+        'Old City House'),
+       ('1-vi Maj Boulevard b.b, Bitola 7000', '41.030889', '21.333390', 'Bakery Boulevard');
 
 
 INSERT INTO public.users (email, name, password, phone_number, role)
-VALUES ('andrej@gmail.com', 'Andrej', '$2a$10$LTObmzdJW46etz1MwbEGS.hOFztzwrf1o09EWu3z4UxgfSWF0Bayy', 'test', 'WAITER'),
-       ('manager@test.com', 'Manager', '$2a$10$LTObmzdJW46etz1MwbEGS.hOFztzwrf1o09EWu3z4UxgfSWF0Bayy', 'manager',
-        'MANAGER');
+VALUES ('manager@test.com', 'Manager', '$2a$10$y35X.5dJGcFz.00jc2QtTujW7BkofmwvJccQrmewJYH8gTc.aQkZC', 'manager',
+        'MANAGER'),
+       ('waiter@test.com', 'Waiter', '$2a$10$sCRTfjiB5JzuPPftNrHEseKAWHQevGTYvxiRdNtz9lkwZOjOn8t0S', 'waiter',
+        'WAITER');
 
-
 INSERT INTO public.tables (id, waiter_id)
-VALUES (1, 1);
-INSERT INTO public.tables (id, waiter_id)
-VALUES (2, null);
-INSERT INTO public.tables (id, waiter_id)
-VALUES (3, null);
-INSERT INTO public.tables (id, waiter_id)
-VALUES (4, null);
-INSERT INTO public.tables (id, waiter_id)
-VALUES (5, null);
-INSERT INTO public.tables (id, waiter_id)
-VALUES (6, 1);
-INSERT INTO public.tables (id, waiter_id)
-VALUES (7, null);
-INSERT INTO public.tables (id, waiter_id)
-VALUES (8, null);
-INSERT INTO public.tables (id, waiter_id)
-VALUES (9, null);
-INSERT INTO public.tables (id, waiter_id)
-VALUES (10, null);
-INSERT INTO public.tables (id, waiter_id)
-VALUES (11, null);
-INSERT INTO public.tables (id, waiter_id)
-VALUES (12, null);
-INSERT INTO public.tables (id, waiter_id)
-VALUES (13, null);
+VALUES (1, 1),
+       (2, null),
+       (3, null),
+       (4, null),
+       (5, null),
+       (6, 1),
+       (7, null),
+       (9, null),
+       (10, null),
+       (11, null),
+       (12, null),
+       (13, null);
